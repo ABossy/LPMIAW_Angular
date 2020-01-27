4680c7cb17d056eba232d70d1d9c38c340576847
ABossy <bossy.astrid@gmail.com>
2020-01-27 17:36:18 +0100

    service+ routing

--- src/app/data/mock-heroes.ts
+++ src/app/data/mock-heroes.ts
@@ -6,9 +6,10 @@ export const HEROES: Hero[] = [
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
   { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 16, name: 'Cloe' },
+  { id: 17, name: 'Lucas' },
+  { id: 18, name: 'Marie' },
+  { id: 19, name: 'Pierro' },
+  { id: 20, name: 'Popo' },
+
 ];
