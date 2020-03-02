a909390713543b47d89c4639c6a3cf9869056b48
ABossy <bossy.astrid@gmail.com>
2020-03-02 11:50:42 +0100

    mise en place de points sur les heros et les armes

--- src/app/creation-armes/creation-armes.component.ts
+++ src/app/creation-armes/creation-armes.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import { Component, DoCheck, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {ArmeService} from '../services/arme.service';
 import {Arme} from '../data/arme';
+import {Hero} from '../data/hero';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-creation-armes',
   templateUrl: './creation-armes.component.html',
   styleUrls: ['./creation-armes.component.css']
 })
-export class CreationArmesComponent implements OnInit {
+export class CreationArmesComponent implements DoCheck, OnInit {
+  arme: Arme;
+  restant: number;
   formArme = new FormGroup({
-    name: new FormControl(''),
-    attaque: new FormControl(''),
-    esquive: new FormControl(''),
-    degats: new FormControl(''),
-    pv: new FormControl(''),
+  name: new FormControl(''),
+  attaque: new FormControl(''),
+  esquive: new FormControl(''),
+  degats: new FormControl(''),
+  pv: new FormControl(''),
   });
-  constructor(private form: FormBuilder,  private armeService: ArmeService) { }
 
+  constructor(private form: FormBuilder,  private armeService: ArmeService, private router: Router) { }
+  ngDoCheck(): void {
+    // tslint:disable-next-line:max-line-length
+    this.restant = 0 - (this.formArme.get('attaque').value + this.formArme.get('esquive').value + this.formArme.get('degats').value + this.formArme.get('pv').value);
+  }
   ngOnInit() {
   }
 
@@ -30,6 +39,8 @@ export class CreationArmesComponent implements OnInit {
     arme.degats = this.formArme.get('degats').value;
     arme.pv = this.formArme.get('pv').value;
     this.armeService.addArme(arme);
+    this.router.navigate(['/armes']);
+
 
   }
 }
--- src/app/creation/creation.component.ts
+++ src/app/creation/creation.component.ts
@@ -1,27 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {Hero} from '../data/hero';
 import {HeroService} from '../services/hero.service';
 import {ArmeService} from '../services/arme.service';
 import {Arme} from '../data/arme';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-creation',
   templateUrl: './creation.component.html',
   styleUrls: ['./creation.component.css']
 })
-export class CreationComponent implements OnInit {
-    formHero = new FormGroup({
-    name: new FormControl(''),
-    attaque: new FormControl(''),
-    esquive: new FormControl(''),
-    degats: new FormControl(''),
-    pv: new FormControl(''),
-    });
+export class CreationComponent implements DoCheck, OnInit {
 
-  constructor(private form: FormBuilder, private heroService: HeroService) { }
+  hero: Hero;
+  arme: Arme;
+  restant: number;
+  formHero = new FormGroup({
+  name: new FormControl(''),
+  attaque: new FormControl(''),
+  esquive: new FormControl(''),
+  degats: new FormControl(''),
+  pv: new FormControl(''),
+  });
 
+
+
+  constructor(private form: FormBuilder, private heroService: HeroService, private router: Router) { }
+
+  ngDoCheck(): void {
+    // tslint:disable-next-line:max-line-length
+    this.restant = 40 - (this.formHero.get('attaque').value + this.formHero.get('esquive').value + this.formHero.get('degats').value + this.formHero.get('pv').value);
+  }
   ngOnInit() {
+    this.hero = new Hero();
+    this.restant = 40;
+    this.formHero.get('attaque').setValue(1);
+    this.formHero.get('esquive').setValue(1);
+    this.formHero.get('degats').setValue(1);
+    this.formHero.get('pv').setValue(1);
+
+    this.arme = new Arme();
+    this.restant = 5;
+
   }
 
   createHero() {
@@ -33,6 +54,7 @@ export class CreationComponent implements OnInit {
     hero.degats = this.formHero.get('degats').value;
     hero.pv = this.formHero.get('pv').value;
     this.heroService.addHero(hero);
+    this.router.navigate(['/heroes']);
 
   }
 
